6ded82f74c7245d949b8a7fa61289723dac3e091
William Castañeda <william.abraham@gmail.com>
2024-08-05 17:38:07 -0600

    first-commit

---
+++ NCo30/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SharpSapRfc;
+
+namespace NCo30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (SapRfcConnection conn = new SapRfcConnection("TST"))
+            {
+                var result = conn.ExecuteFunction("Z_SSRT_SUM", new
+                {
+                    i_num1 = 2,
+                    i_num2 = 4
+                });
+
+                int xresult = result.GetOutput<int>("e_result");
+            }
+        }
+    }
+}
